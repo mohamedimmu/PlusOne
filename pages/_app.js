3e5cfde0d866fa8b0bb0816ffd100b18dac62d5a
unknown <developercode011@gmail.com>
2022-05-19 21:16:51 +0530

    version 1

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Layout } from "../components";
+import { useRouter } from "next/router";
+import { StateContext } from '../context/StateContext';
+import { Toaster } from 'react-hot-toast'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} key={router.asPath} />
+      </Layout>
+    </StateContext>
+  );
 }
 
-export default MyApp
+export default MyApp;
